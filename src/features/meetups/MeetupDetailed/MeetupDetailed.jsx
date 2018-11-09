b59b44ee9dfd7858e94733d1f5dd4446622d03e6
Muhammad Alam <alam@mmtutor.com>
2018-11-09 02:50:48 -0500

    add placeholder files

---
+++ src/features/meetups/MeetupDetailed/MeetupDetailed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MeetupDetailedPage = () => {
+  return (
+    <div>
+      <h1>Meetup Detailed Page</h1>
+    </div>
+  );
+};
+
+export default MeetupDetailedPage;
---
+++ src/features/user/Settings/PhotoPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PhotoPage = () => {
+  return (
+    <div>
+      <h1>PhotoPage</h1>
+    </div>
+  );
+};
+
+export default PhotoPage;
